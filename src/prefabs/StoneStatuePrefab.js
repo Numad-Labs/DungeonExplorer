7b545dc296bd43f3b9c0b16b16d8d4e50517798c
namkhai-dev <b.namhai21@gmail.com>
2025-05-05 13:20:06 +0800

    main map bug fixed

---
+++ src/prefabs/StoneStatuePrefab.js
@@ -0,0 +1,320 @@
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class StoneStatuePrefab extends Phaser.GameObjects.Container {
+
+	constructor(scene, x, y) {
+		super(scene, x ?? 1009, y ?? 1208);
+
+		this.blendMode = Phaser.BlendModes.SKIP_CHECK;
+
+		// rock_pillars_coming_from_darkness_bg_00
+		const rock_pillars_coming_from_darkness_bg_00 = scene.add.image(14, 124, "rock pillars coming from darkness-bg_0", 0);
+		this.add(rock_pillars_coming_from_darkness_bg_00);
+
+		// statues_far_from_platforms_bg_40
+		const statues_far_from_platforms_bg_40 = scene.add.image(16, -18, "statues-far from platforms-bg_4", 0);
+		this.add(statues_far_from_platforms_bg_40);
+
+		// crystals1_improved_refraction_80
+		const crystals1_improved_refraction_80 = scene.add.image(-12, 71, "Crystals1-improved refraction_8", 0);
+		this.add(crystals1_improved_refraction_80);
+
+		// crystals1_improved_refraction_110
+		const crystals1_improved_refraction_110 = scene.add.image(5, 86, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_110);
+
+		// crystals1_improved_refraction_60
+		const crystals1_improved_refraction_60 = scene.add.image(27, 87, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_60);
+
+		// crystals1_improved_refraction
+		const crystals1_improved_refraction = scene.add.image(42, 71, "Crystals1-improved refraction_8", 0);
+		this.add(crystals1_improved_refraction);
+
+		// crystals1_improved_refraction_1
+		const crystals1_improved_refraction_1 = scene.add.image(-22, 97, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_1);
+
+		// crystals1_improved_refraction_2
+		const crystals1_improved_refraction_2 = scene.add.image(4, 111, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_2);
+
+		// crystals1_improved_refraction_3
+		const crystals1_improved_refraction_3 = scene.add.image(29, 117, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_3);
+
+		// crystals1_improved_refraction_4
+		const crystals1_improved_refraction_4 = scene.add.image(-24, 119, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_4);
+
+		// crystals1_improved_refraction_5
+		const crystals1_improved_refraction_5 = scene.add.image(-31, 148, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_5);
+
+		// crystals1_improved_refraction_6
+		const crystals1_improved_refraction_6 = scene.add.image(2, 139, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_6);
+
+		// crystals1_improved_refraction_7
+		const crystals1_improved_refraction_7 = scene.add.image(24, 139, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_7);
+
+		// crystals1_improved_refraction_8
+		const crystals1_improved_refraction_8 = scene.add.image(48, 96, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_8);
+
+		// crystals1_improved_refraction_9
+		const crystals1_improved_refraction_9 = scene.add.image(57, 119, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_9);
+
+		// crystals1_improved_refraction_10
+		const crystals1_improved_refraction_10 = scene.add.image(41, 143, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_10);
+
+		// crystals1_improved_refraction_11
+		const crystals1_improved_refraction_11 = scene.add.image(64, 151, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_11);
+
+		// crystals1_improved_refraction_12
+		const crystals1_improved_refraction_12 = scene.add.image(-15, 154, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_12);
+
+		// crystals1_improved_refraction_13
+		const crystals1_improved_refraction_13 = scene.add.image(-31, 176, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_13);
+
+		// crystals1_improved_refraction_14
+		const crystals1_improved_refraction_14 = scene.add.image(-9, 192, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_14);
+
+		// crystals1_improved_refraction_15
+		const crystals1_improved_refraction_15 = scene.add.image(18, 200, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_15);
+
+		// crystals1_improved_refraction_16
+		const crystals1_improved_refraction_16 = scene.add.image(39, 196, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_16);
+
+		// crystals1_improved_refraction_17
+		const crystals1_improved_refraction_17 = scene.add.image(66, 175, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_17);
+
+		// crystals1_improved_refraction_18
+		const crystals1_improved_refraction_18 = scene.add.image(2, 169, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_18);
+
+		// crystals1_improved_refraction_19
+		const crystals1_improved_refraction_19 = scene.add.image(35, 167, "Crystals1-improved refraction_8", 0);
+		this.add(crystals1_improved_refraction_19);
+
+		// crystals1_improved_refraction_20
+		const crystals1_improved_refraction_20 = scene.add.image(14, 60, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_20);
+
+		// crystals1_improved_refraction_21
+		const crystals1_improved_refraction_21 = scene.add.image(-13, 50, "Crystals1-improved refraction_11", 0);
+		this.add(crystals1_improved_refraction_21);
+
+		// crystals1_improved_refraction_22
+		const crystals1_improved_refraction_22 = scene.add.image(33, 54, "Crystals1-improved refraction_6", 0);
+		this.add(crystals1_improved_refraction_22);
+
+		this.rock_pillars_coming_from_darkness_bg_00 = rock_pillars_coming_from_darkness_bg_00;
+		this.statues_far_from_platforms_bg_40 = statues_far_from_platforms_bg_40;
+		this.crystals1_improved_refraction_80 = crystals1_improved_refraction_80;
+		this.crystals1_improved_refraction_110 = crystals1_improved_refraction_110;
+		this.crystals1_improved_refraction_60 = crystals1_improved_refraction_60;
+		this.crystals1_improved_refraction = crystals1_improved_refraction;
+		this.crystals1_improved_refraction_1 = crystals1_improved_refraction_1;
+		this.crystals1_improved_refraction_2 = crystals1_improved_refraction_2;
+		this.crystals1_improved_refraction_3 = crystals1_improved_refraction_3;
+		this.crystals1_improved_refraction_4 = crystals1_improved_refraction_4;
+		this.crystals1_improved_refraction_5 = crystals1_improved_refraction_5;
+		this.crystals1_improved_refraction_6 = crystals1_improved_refraction_6;
+		this.crystals1_improved_refraction_7 = crystals1_improved_refraction_7;
+		this.crystals1_improved_refraction_8 = crystals1_improved_refraction_8;
+		this.crystals1_improved_refraction_9 = crystals1_improved_refraction_9;
+		this.crystals1_improved_refraction_10 = crystals1_improved_refraction_10;
+		this.crystals1_improved_refraction_11 = crystals1_improved_refraction_11;
+		this.crystals1_improved_refraction_12 = crystals1_improved_refraction_12;
+		this.crystals1_improved_refraction_13 = crystals1_improved_refraction_13;
+		this.crystals1_improved_refraction_14 = crystals1_improved_refraction_14;
+		this.crystals1_improved_refraction_15 = crystals1_improved_refraction_15;
+		this.crystals1_improved_refraction_16 = crystals1_improved_refraction_16;
+		this.crystals1_improved_refraction_17 = crystals1_improved_refraction_17;
+		this.crystals1_improved_refraction_18 = crystals1_improved_refraction_18;
+		this.crystals1_improved_refraction_19 = crystals1_improved_refraction_19;
+		this.crystals1_improved_refraction_20 = crystals1_improved_refraction_20;
+		this.crystals1_improved_refraction_21 = crystals1_improved_refraction_21;
+		this.crystals1_improved_refraction_22 = crystals1_improved_refraction_22;
+
+		/* START-USER-CTR-CODE */
+        // Setup physics for collision
+        scene.physics.add.existing(this, false);
+
+        this.body = this.body || {};
+        this.body.allowGravity = false;
+        this.body.setSize(120, 75); 
+        this.body.setOffset(-45, 135);
+        this.body.moves = false;
+        this.body.immovable = true;
+
+        this.isStatueInvisible = false;
+        this.isPlayerInvisible = false;
+
+        this.statueTop = this.y - 95;
+        this.statueBottom = this.y + 200;
+
+        // Set up update event
+        this.updateListener = () => this.checkPlayerPosition();
+        scene.events.on('update', this.updateListener);
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @type {Phaser.GameObjects.Image} */
+	rock_pillars_coming_from_darkness_bg_00;
+	/** @type {Phaser.GameObjects.Image} */
+	statues_far_from_platforms_bg_40;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_80;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_110;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_60;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_1;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_2;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_3;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_4;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_5;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_6;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_7;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_8;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_9;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_10;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_11;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_12;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_13;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_14;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_15;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_16;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_17;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_18;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_19;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_20;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_21;
+	/** @type {Phaser.GameObjects.Image} */
+	crystals1_improved_refraction_22;
+
+	/* START-USER-CODE */
+
+    setupCollision(player) {
+        if (!this.scene || !player) return;
+
+        this.scene.physics.add.collider(player, this, null, null, this);
+        this.player = player;
+    }
+
+    checkPlayerPosition() {
+        if (!this.player || !this.scene) return;
+
+        const playerNearStatueX = Math.abs(this.player.x + 10 - this.x - 25) < 55;
+
+        const playerBehindStatue = this.player.y > this.statueTop && 
+                                  this.player.y < this.statueBottom && 
+                                  playerNearStatueX;
+
+        if (playerBehindStatue) {
+            if (!this.isStatueInvisible) {
+                this.scene.tweens.add({
+                    targets: this,
+                    alpha: 0.4,
+                    duration: 200,
+                    ease: 'Power1',
+                    onComplete: () => {
+                        this.isStatueInvisible = true;
+                    }
+                });
+            }
+
+
+            if (!this.isPlayerInvisible) {
+                this.scene.tweens.add({
+                    targets: this.player,
+                    alpha: 0.3,
+                    duration: 200,
+                    ease: 'Power1',
+                    onComplete: () => {
+                        this.isPlayerInvisible = true;
+                    }
+                });
+            }
+        } else {
+
+            if (this.isStatueInvisible) {
+                this.scene.tweens.add({
+                    targets: this,
+                    alpha: 1,
+                    duration: 200,
+                    ease: 'Power1',
+                    onComplete: () => {
+                        this.isStatueInvisible = false;
+                    }
+                });
+            }
+
+
+            if (this.isPlayerInvisible) {
+                this.scene.tweens.add({
+                    targets: this.player,
+                    alpha: 1,
+                    duration: 200,
+                    ease: 'Power1',
+                    onComplete: () => {
+                        this.isPlayerInvisible = false;
+                    }
+                });
+            }
+        }
+    }
+
+
+    destroy() {
+        if (this.scene) {
+            this.scene.events.off('update', this.updateListener);
+        }
+        super.destroy();
+    }
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
--- src/scenes/MainMapScene.js
+++ src/scenes/MainMapScene.js
@@ -5,6 +5,7 @@
 
 import BaseGameScene from "./BaseGameScene";
 import PlayerPrefab from "../prefabs/PlayerPrefab";
+import StoneStatuePrefab from "../prefabs/StoneStatuePrefab";
 /* START-USER-IMPORTS */
 /* END-USER-IMPORTS */
 
@@ -45,26 +46,17 @@ export default class MainMapScene extends BaseGameScene {
 		mainMap.addTilesetImage("platform1", "platform1");
 		mainMap.addTilesetImage("lavafal-for Tiled-spritesheet-16frames", "lavafal-for Tiled-spritesheet-16frames");
 
-		// map_Col_1
-		const map_Col_1 = mainMap.createLayer("Map_Col", ["Bed"], 0, 0);
-
 		// backGround_1
 		const backGround_1 = mainMap.createLayer("BackGround", ["Wall_3"], 0, 0);
+
+		// map_Col_1
+		const map_Col_1 = mainMap.createLayer("Map_Col", ["Bed"], 0, 0);
 
 		// backGround
 		const backGround = mainMap.createLayer("BackGround_1", ["lavafal-for Tiled-spritesheet-16frames"], 0, 0);
 
-		// hG_1
-		const hG_1 = mainMap.createLayer("HG", ["rock pillars coming from darkness-bg_2"], 0, 0);
-
-		// stone_Statue_1
-		const stone_Statue_1 = mainMap.createLayer("Stone_Statue", ["statues-far from platforms-bg_5","statues-far from platforms-bg_2","statues-far from platforms-bg_4","statues-far from platforms-bg_1"], 0, 0);
-
 		// wall_Upper_1
 		const wall_Upper_1 = mainMap.createLayer("Wall_Upper", ["Tileset 3"], 16, 12);
-
-		// door_1
-		const door_1 = mainMap.createLayer("Door", ["side way-Sheet"], 0, 0);
 
 		// wall_down_1
 		const wall_down_1 = mainMap.createLayer("Wall_down", ["Tileset 3"], -16, 13);
@@ -77,9 +69,6 @@ export default class MainMapScene extends BaseGameScene {
 
 		// wall_RD_1
 		const wall_RD_1 = mainMap.createLayer("Wall_RD", ["Tileset 3"], -15, -19);
-
-		// map_AC
-		const map_AC = mainMap.createLayer("Map_AC_1", ["Crystals1-improved refraction_6","Crystals1-improved refraction_11","Crystals1-improved refraction_8"], 0, 0);
 
 		// gold_AC_1
 		const gold_AC_1 = mainMap.createLayer("Gold_AC", ["Gold"], -14, -19);
@@ -96,51 +85,106 @@ export default class MainMapScene extends BaseGameScene {
 		// vase_AC_1
 		const vase_AC_1 = mainMap.createLayer("Vase_AC", ["Atlas-props"], 0, 0);
 
-		// torch_1
-		const torch_1 = mainMap.createLayer("Torch", ["Fire-candelabrum"], 34, 0);
-
 		// playerPrefab
-		const playerPrefab = new PlayerPrefab(this, 959, 1221);
+		const playerPrefab = new PlayerPrefab(this, 900, 100);
 		this.add.existing(playerPrefab);
 
-		this.map_Col_1 = map_Col_1;
+		// stoneStatuePrefab
+		const stoneStatuePrefab = new StoneStatuePrefab(this, 944, 869);
+		this.add.existing(stoneStatuePrefab);
+
+		// stoneStatuePrefab_1
+		const stoneStatuePrefab_1 = new StoneStatuePrefab(this, 944, 1417);
+		this.add.existing(stoneStatuePrefab_1);
+
+		// stoneStatuePrefab_2
+		const stoneStatuePrefab_2 = new StoneStatuePrefab(this, 1592, 1417);
+		this.add.existing(stoneStatuePrefab_2);
+
+		// stoneStatuePrefab_3
+		const stoneStatuePrefab_3 = new StoneStatuePrefab(this, 1592, 869);
+		this.add.existing(stoneStatuePrefab_3);
+
+		// torchAnim
+		const torchAnim = this.add.sprite(734, 1993, "Fire-candelabrum", 0);
+		torchAnim.play("TorchAnim");
+
+		// torchAnim_1
+		const torchAnim_1 = this.add.sprite(1253, 1993, "Fire-candelabrum", 0);
+		torchAnim_1.play("TorchAnim");
+
+		// torchAnim_2
+		const torchAnim_2 = this.add.sprite(1799, 1993, "Fire-candelabrum", 0);
+		torchAnim_2.play("TorchAnim");
+
+		// torchAnim_3
+		const torchAnim_3 = this.add.sprite(2017, 1737, "Fire-candelabrum", 0);
+		torchAnim_3.play("TorchAnim");
+
+		// torchAnim_4
+		const torchAnim_4 = this.add.sprite(2017, 1231, "Fire-candelabrum", 0);
+		torchAnim_4.play("TorchAnim");
+
+		// torchAnim_5
+		const torchAnim_5 = this.add.sprite(2017, 656, "Fire-candelabrum", 0);
+		torchAnim_5.play("TorchAnim");
+
+		// torchAnim_6
+		const torchAnim_6 = this.add.sprite(514, 1737, "Fire-candelabrum", 0);
+		torchAnim_6.play("TorchAnim");
+
+		// torchAnim_7
+		const torchAnim_7 = this.add.sprite(514, 1231, "Fire-candelabrum", 0);
+		torchAnim_7.play("TorchAnim");
+
+		// torchAnim_8
+		const torchAnim_8 = this.add.sprite(514, 656, "Fire-candelabrum", 0);
+		torchAnim_8.play("TorchAnim");
+
+		// torchAnim_9
+		const torchAnim_9 = this.add.sprite(770, 485, "Fire-candelabrum", 0);
+		torchAnim_9.play("TorchAnim");
+
+		// torchAnim_10
+		const torchAnim_10 = this.add.sprite(1283, 485, "Fire-candelabrum", 0);
+		torchAnim_10.play("TorchAnim");
+
+		// torchAnim_11
+		const torchAnim_11 = this.add.sprite(1831, 485, "Fire-candelabrum", 0);
+		torchAnim_11.play("TorchAnim");
+
 		this.backGround_1 = backGround_1;
+		this.map_Col_1 = map_Col_1;
 		this.backGround = backGround;
-		this.hG_1 = hG_1;
-		this.stone_Statue_1 = stone_Statue_1;
 		this.wall_Upper_1 = wall_Upper_1;
-		this.door_1 = door_1;
 		this.wall_down_1 = wall_down_1;
 		this.wall_RU_1 = wall_RU_1;
 		this.survive_Zone_1 = survive_Zone_1;
 		this.wall_RD_1 = wall_RD_1;
-		this.map_AC = map_AC;
 		this.gold_AC_1 = gold_AC_1;
 		this.map = map;
 		this.teleportation_1 = teleportation_1;
 		this.bed_1 = bed_1;
 		this.vase_AC_1 = vase_AC_1;
-		this.torch_1 = torch_1;
 		this.playerPrefab = playerPrefab;
+		this.stoneStatuePrefab = stoneStatuePrefab;
+		this.stoneStatuePrefab_1 = stoneStatuePrefab_1;
+		this.stoneStatuePrefab_2 = stoneStatuePrefab_2;
+		this.stoneStatuePrefab_3 = stoneStatuePrefab_3;
+		this.torchAnim = torchAnim;
 		this.mainMap = mainMap;
 
 		this.events.emit("scene-awake");
 	}
 
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
-	map_Col_1;
-	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	backGround_1;
+	/** @type {Phaser.Tilemaps.TilemapLayer} */
+	map_Col_1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	backGround;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
-	hG_1;
-	/** @type {Phaser.Tilemaps.TilemapLayer} */
-	stone_Statue_1;
-	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	wall_Upper_1;
-	/** @type {Phaser.Tilemaps.TilemapLayer} */
-	door_1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	wall_down_1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
@@ -149,8 +193,6 @@ export default class MainMapScene extends BaseGameScene {
 	survive_Zone_1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	wall_RD_1;
-	/** @type {Phaser.Tilemaps.TilemapLayer} */
-	map_AC;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	gold_AC_1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
@@ -161,58 +203,58 @@ export default class MainMapScene extends BaseGameScene {
 	bed_1;
 	/** @type {Phaser.Tilemaps.TilemapLayer} */
 	vase_AC_1;
-	/** @type {Phaser.Tilemaps.TilemapLayer} */
-	torch_1;
 	/** @type {PlayerPrefab} */
 	playerPrefab;
+	/** @type {StoneStatuePrefab} */
+	stoneStatuePrefab;
+	/** @type {StoneStatuePrefab} */
+	stoneStatuePrefab_1;
+	/** @type {StoneStatuePrefab} */
+	stoneStatuePrefab_2;
+	/** @type {StoneStatuePrefab} */
+	stoneStatuePrefab_3;
+	/** @type {Phaser.GameObjects.Sprite} */
+	torchAnim;
 	/** @type {Phaser.Tilemaps.Tilemap} */
 	mainMap;
 
 	/* START-USER-CODE */
 
 	create() {
+        this.cameras.main.setBounds(0, 0, 2560, 2560);
+		this.physics.world.bounds.width = 1000;
+		this.physics.world.bounds.height = 800;
+
 		try {
-			// Create editor objects
 			this.editorCreate();
-
-			// Setup collision layers
 			this.setupCollisions();
-
-			// Initialize player reference for easier access
 			this.player = this.playerPrefab;
-
-			// Initialize game managers (from BaseGameScene)
 			this.initializeManagers();
-
-			// Setup player attack system (from BaseGameScene)
 			this.setupPlayerAttack();
-
-			// Setup teleportation zones
 			this.setupTeleportation();
-
-			// Setup test controls for development (from BaseGameScene)
 			this.setupTestControls();
-
 			console.log("MainMapScene created successfully");
 		} catch (error) {
 			console.error("Error in MainMapScene create:", error);
 		}
-	}
+		this.stoneStatuePrefab.setupCollision(this.playerPrefab)
+		this.stoneStatuePrefab_1.setupCollision(this.playerPrefab)
+		this.stoneStatuePrefab_2.setupCollision(this.playerPrefab)
+		this.stoneStatuePrefab_3.setupCollision(this.playerPrefab)
 
-	// Setup map collisions
+		this.physics.add.collider(this.playerPrefab, this.map_Col_1);
+		this.map_Col_1.setCollisionBetween(0, 10000);
+
+		this.physics.add.collider(this.playerPrefab, this.backGround);
+		this.backGround.setCollisionBetween(0, 10000);
+	}
 	setupCollisions() {
 		try {
-			// Set collision for main terrain layers
 			this.physics.add.collider(this.playerPrefab, this.hG_1);
 			this.hG_1.setCollisionBetween(0, 10000);
 
-			this.physics.add.collider(this.playerPrefab, this.map_Col_1);
-			this.map_Col_1.setCollisionBetween(0, 10000);
-
 			this.physics.add.collider(this.playerPrefab, this.backGround);
 			this.backGround.setCollisionBetween(0, 10000);
-
-			// Optionally enable debug view for collisions during development
 			if (process.env.NODE_ENV !== 'production') {
 				// Uncomment to show debug collision boxes
 				// this.hG_1.renderDebug(this.add.graphics());
@@ -226,33 +268,20 @@ export default class MainMapScene extends BaseGameScene {
 		}
 	}
 
-	// Setup teleportation zones for map transitions
 	setupTeleportation() {
 		try {
-			// Check if there is a teleportation layer to use
 			if (this.teleportation_1) {
-				// Create a physics object for each teleport point
-				// This is a placeholder - you'll need to implement your specific teleportation logic
 				const teleporters = [];
-
-				// Example: Search for teleport points in the teleportation layer
-				// Note: This depends on how you've set up your teleport tiles in the map
 				this.teleportation_1.forEachTile(tile => {
-					if (tile.index !== -1) { // Not an empty tile
-						// Create a zone at this tile's position
+					if (tile.index !== -1) { 
 						const x = tile.x * tile.width + (tile.width / 2);
 						const y = tile.y * tile.height + (tile.height / 2);
 
 						const teleportZone = this.add.zone(x, y, tile.width, tile.height);
 						this.physics.world.enable(teleportZone);
 						teleporters.push(teleportZone);
-
-						// Store destination data if available in tile properties
-						// Example: tile.properties.destX, tile.properties.destY, tile.properties.destMap
 					}
 				});
-
-				// Add collision for all teleport zones
 				if (teleporters.length > 0) {
 					this.physics.add.overlap(this.player, teleporters, this.handleTeleportZone, null, this);
 				}
@@ -264,34 +293,18 @@ export default class MainMapScene extends BaseGameScene {
 		}
 	}
 
-	// Handle teleportation when player overlaps a teleport zone
 	handleTeleportZone(player, teleportZone) {
-		// Get destination scene (could be stored in teleportZone.data)
-		const destScene = "FirstArea"; // Default destination
-
-		// Use the base class teleport handler
+		const destScene = "FirstArea";
 		this.handleTeleport(player, teleportZone, destScene);
 	}
-
 	setupTestControls() {
-		// Call the base class implementation
 		super.setupTestControls();
-
-		// Add scene-specific test controls here if needed
 	}
-
 	update(time, delta) {
-		// Call the base class update method
 		super.update(time, delta);
-
-		// Add scene-specific update logic here if needed
 	}
-
 	shutdown() {
-		// Call the base class shutdown method
 		super.shutdown();
-
-		// Add scene-specific cleanup here if needed
 	}
 
 	/* END-USER-CODE */
